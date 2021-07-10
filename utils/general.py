c988002950c9f71eb463cb381351e41c766bfd38
Junjie <junjierao2016@gmail.com>
2021-07-10 15:00:09 +0100

    hw03 first commit

--- utils/general.py
+++ utils/general.py
@@ -1,4 +1,4 @@
-from keras.utils import plot_model
+from tensorflow.keras.utils import plot_model
 from IPython.display import Image
 import random
 import math
@@ -6,6 +6,8 @@ import string
 import os
 
 # Display Keras Model
+
+
 def show_keras_model(model):
     filename = "".join(random.choices(string.ascii_uppercase, k=10)) + ".png"
     plot_model(model, to_file=filename, show_shapes=True, show_layer_names=True)
@@ -14,8 +16,11 @@ def show_keras_model(model):
     return image
 
 # sigmoid
+
+
 def sigmoid(x):
     return 1.0 / (1 + math.exp(-x))
+
 
 def tanh(x):
     return math.tanh(x)
